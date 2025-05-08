c273b78f662064712ffd6da9a6ca3268fd3201a2
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-05-08 14:54:18 +0530

    Update upload_orders.php

--- ajax/upload_orders.php
+++ ajax/upload_orders.php
@@ -1,5 +1,9 @@
 <?php
 // ajax/upload_orders.php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 header('Content-Type: application/json');
 require_once '../vendor/autoload.php';
 include '../inc/db.php';
