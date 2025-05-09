e5a6bb7d214687ad3eb616a3a1e03867af8e25ad
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-05-09 10:56:41 +0530

    Update customers.php

--- customers.php
+++ customers.php
@@ -90,29 +90,33 @@ include 'inc/sidebar.php';
                     <table class="table table-bordered table-striped table-hover">
                         <thead class="table-dark">
                             <tr>
+                                <th style="width: 40px;">#</th>
                                 <th>Name</th>
                                 <th>Phone</th>
                                 <th>Email</th>
                                 <th>GST</th>
-                                <th>Actions</th>
+                                <th style="width: 180px;">Actions</th>
                             </tr>
                         </thead>
                         <tbody id="customerTableBody">
                             <?php
-                            $query = "SELECT * FROM customers";
+                            $query = "SELECT * FROM customers ORDER BY id DESC";
                             $result = mysqli_query($conn, $query);
+                            $sr = 1;
                             while ($row = mysqli_fetch_assoc($result)) {
                                 echo "<tr>";
-                                echo "<td>{$row['name']}</td>";
-                                echo "<td>{$row['phone']}</td>";
-                                echo "<td>{$row['email']}</td>";
-                                echo "<td>{$row['gst_no']}</td>";
+                                echo "<td>{$sr}</td>";
+                                echo "<td>".htmlspecialchars($row['name'])."</td>";
+                                echo "<td>".htmlspecialchars($row['phone'])."</td>";
+                                echo "<td>".htmlspecialchars($row['email'])."</td>";
+                                echo "<td>".htmlspecialchars($row['gst_no'])."</td>";
                                 echo "<td>";
-                                echo "<button type='button' class='btn btn-info view-details-btn' data-id='{$row['id']}'>View</button> ";
-                                echo "<button type='button' class='btn btn-warning edit-btn' data-id='{$row['id']}' data-name='{$row['name']}' data-address='{$row['address']}' data-phone='{$row['phone']}' data-email='{$row['email']}' data-gst='{$row['gst_no']}' data-hsn='{$row['hsn_code']}' data-pan='{$row['pan_no']}' data-cin='{$row['cin_no']}' data-aadhaar='{$row['aadhaar_no']}' data-destination='{$row['destination']}' data-parcel_type='{$row['parcel_type']}' data-weight='{$row['weight']}' data-price='{$row['price']}'>Edit</button> ";
-                                echo "<button type='button' class='btn btn-danger delete-btn' data-id='{$row['id']}'>Delete</button>";
+                                echo "<button type='button' class='btn btn-info btn-sm view-details-btn me-1' data-id='{$row['id']}'>View</button> ";
+                                echo "<button type='button' class='btn btn-warning btn-sm edit-btn me-1' data-id='{$row['id']}' data-name='".htmlspecialchars($row['name'], ENT_QUOTES)."' data-address='".htmlspecialchars($row['address'], ENT_QUOTES)."' data-phone='".htmlspecialchars($row['phone'], ENT_QUOTES)."' data-email='".htmlspecialchars($row['email'], ENT_QUOTES)."' data-gst='".htmlspecialchars($row['gst_no'], ENT_QUOTES)."' data-hsn='".htmlspecialchars($row['hsn_code'], ENT_QUOTES)."' data-pan='".htmlspecialchars($row['pan_no'], ENT_QUOTES)."' data-cin='".htmlspecialchars($row['cin_no'], ENT_QUOTES)."' data-aadhaar='".htmlspecialchars($row['aadhaar_no'], ENT_QUOTES)."' data-destination='".htmlspecialchars($row['destination'], ENT_QUOTES)."' data-parcel_type='".htmlspecialchars($row['parcel_type'], ENT_QUOTES)."' data-weight='".htmlspecialchars($row['weight'], ENT_QUOTES)."' data-price='".htmlspecialchars($row['price'], ENT_QUOTES)."'>Edit</button> ";
+                                echo "<button type='button' class='btn btn-danger btn-sm delete-btn' data-id='{$row['id']}'>Delete</button>";
                                 echo "</td>";
                                 echo "</tr>";
+                                $sr++;
                             }
                             ?>
                         </tbody>
