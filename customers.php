79106a16262dbcc910a452891049c6dc91b20a7c
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2025-05-09 10:59:23 +0530

    Update customers.php

--- customers.php
+++ customers.php
@@ -99,26 +99,7 @@ include 'inc/sidebar.php';
                             </tr>
                         </thead>
                         <tbody id="customerTableBody">
-                            <?php
-                            $query = "SELECT * FROM customers ORDER BY id DESC";
-                            $result = mysqli_query($conn, $query);
-                            $sr = 1;
-                            while ($row = mysqli_fetch_assoc($result)) {
-                                echo "<tr>";
-                                echo "<td>{$sr}</td>";
-                                echo "<td>".htmlspecialchars($row['name'])."</td>";
-                                echo "<td>".htmlspecialchars($row['phone'])."</td>";
-                                echo "<td>".htmlspecialchars($row['email'])."</td>";
-                                echo "<td>".htmlspecialchars($row['gst_no'])."</td>";
-                                echo "<td>";
-                                echo "<button type='button' class='btn btn-info btn-sm view-details-btn me-1' data-id='{$row['id']}'>View</button> ";
-                                echo "<button type='button' class='btn btn-warning btn-sm edit-btn me-1' data-id='{$row['id']}' data-name='".htmlspecialchars($row['name'], ENT_QUOTES)."' data-address='".htmlspecialchars($row['address'], ENT_QUOTES)."' data-phone='".htmlspecialchars($row['phone'], ENT_QUOTES)."' data-email='".htmlspecialchars($row['email'], ENT_QUOTES)."' data-gst='".htmlspecialchars($row['gst_no'], ENT_QUOTES)."' data-hsn='".htmlspecialchars($row['hsn_code'], ENT_QUOTES)."' data-pan='".htmlspecialchars($row['pan_no'], ENT_QUOTES)."' data-cin='".htmlspecialchars($row['cin_no'], ENT_QUOTES)."' data-aadhaar='".htmlspecialchars($row['aadhaar_no'], ENT_QUOTES)."' data-destination='".htmlspecialchars($row['destination'], ENT_QUOTES)."' data-parcel_type='".htmlspecialchars($row['parcel_type'], ENT_QUOTES)."' data-weight='".htmlspecialchars($row['weight'], ENT_QUOTES)."' data-price='".htmlspecialchars($row['price'], ENT_QUOTES)."'>Edit</button> ";
-                                echo "<button type='button' class='btn btn-danger btn-sm delete-btn' data-id='{$row['id']}'>Delete</button>";
-                                echo "</td>";
-                                echo "</tr>";
-                                $sr++;
-                            }
-                            ?>
+                            <!-- Data will be loaded here via AJAX -->
                         </tbody>
                     </table>
                 </div>
